d7239e0628a946f033a78d7f5b739ecefd236a26
Wei <weiren.xie@luvago.com>
2016-02-21 11:47:28 +0100

    welcome

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.end('welcome to loko\n');
 }).listen(3000, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:3000/');
