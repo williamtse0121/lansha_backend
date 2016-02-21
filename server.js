d2b0da0bdfb814eb780afbaa28d7cdd8347a6d02
Wei <weiren.xie@luvago.com>
2016-02-21 10:16:57 +0100

    port edit

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 var http = require('http');
 
-http.createServer(function (request, response) {
-
-	// 发送 HTTP 头部 
-	// HTTP 状态值: 200 : OK
-	// 内容类型: text/plain
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-
-	// 发送响应数据 "Hello World"
-	response.end('Hello World\n');
-}).listen(8000);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8000/');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(3000, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:3000/');
